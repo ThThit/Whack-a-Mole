afeaecbca69b3cc00b66b3cd281b4f08548387f0
TH_Thit <thuhtinthit@gmail.com>
2026-01-02 12:19:38 +0700

    implement game start and restart btns

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 const bonk = document.getElementById('soundBonk');
 const gameBoard = document.querySelector('.game-board');
 const moles = document.querySelectorAll('.mole');
+const btnGameReset = document.getElementById('gameReset');
+const btnGameStart = document.getElementById('gameStart');
+
+let gameInterval;
+
+function showAllMoles() {
+    moles.forEach(mole => mole.classList.add('up'));
+}
+
+showAllMoles();
 
 // random mole
-
 function randomMole() {
     moles.forEach(mole => {
-        mole.textContent = '';
+        // hid moles
+        mole.classList.remove('up');
     });
 
     // pick random index 
     const ranIndx = Math.floor(Math.random() * moles.length);
 
-    moles[ranIndx].textContent = "Mole";
+    moles[ranIndx].classList.add('up');
 }
 
-setInterval(randomMole, 800);
+btnGameStart.addEventListener('click', () => {
+    // prevent multival input
+    clearInterval(gameInterval);
+
+    // mole apper every 600 milisecond
+    setTimeout(() => {
+        gameInterval = setInterval(randomMole, 600);
+    });
+});
+
+btnGameReset.addEventListener('click', () => {
+    clearInterval(gameInterval);
+    showAllMoles();
+})
 
 
 // hammer sfx when clicked
